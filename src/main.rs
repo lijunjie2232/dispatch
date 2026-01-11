7f9e70db87516359ffecdebf7af61c4aa231c77b
lijunjie2232 <2533584225@qq.com>
2026-01-11 12:58:48 +0900

    add config arg

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,9 @@ use clap::Parser;
 use debug::LogStrategy;
 use dispatcher::{RawWeightedAddress, WeightedAddress};
 use eyre::Result;
+use tracing::{info, warn};
 
+mod config;
 mod debug;
 mod dispatcher;
 mod list;
@@ -28,15 +30,17 @@ enum Command {
     /// Lists all available network interfaces
     List,
     /// Starts the SOCKS proxy server
-    Start {
+    Balance {
         /// Which IP to accept connections from
-        #[arg(default_value = "127.0.0.1", long)]
+        #[arg(default_value = "127.0.0.1", long, short = 'i')]
         ip: IpAddr,
         /// Which port to listen to for connections
-        #[arg(default_value = "1080", long)]
+        #[arg(default_value = "1080", long, short = 'p')]
         port: u16,
+        #[arg(long, short = 'c')]
+        config: String,
         /// The network interface IP addresses to dispatch to, in the form of <address>[/priority]
-        #[arg(required = true, value_parser = RawWeightedAddress::from_str)]
+        #[arg(value_parser = RawWeightedAddress::from_str)]
         addresses: Vec<RawWeightedAddress>,
     },
 }
@@ -52,12 +56,32 @@ fn main() -> Result<()> {
 
     match opt.command {
         Command::List => list::list(),
-        Command::Start {
-            ip,
-            port,
-            addresses,
+        Command::Balance {
+            mut ip,
+            mut port,
+            config,
+            mut addresses,
         } => {
+            // 如果没有通过命令行提供addresses，则尝试从配置文件读取
+            if !config.is_empty() {
+                info!("Using config file: {}", config);
+
+                addresses = config::load_addresses_from_config(&config)?;
+
+                if let Some(config_ip) = config::load_ip_from_config(&config)? {
+                    ip = config_ip;
+                    warn!("Using IP from config file: {}", ip);
+                }
+
+                if let Some(config_port) = config::load_port_from_config(&config)? {
+                    port = config_port;
+                    warn!("Using port from config file: {}", port);
+                }
+            } else {
+                info!("No config file provided, parsing sys args..");
+            }
             let addresses = WeightedAddress::resolve(addresses)?;
+
             server::server(ip, port, addresses)?
         }
     }
